98c58c13cfdc17122f7087101f5b8bcf86635b01
Andrew Wason <rectalogic@rectalogic.com>
2025-09-01 16:35:46 -0400

    clean up some legacy cruft. implement restaurants.json

--- bin/jekyll.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-MOUNT="$(cd "${BASH_SOURCE%/*}/.."; pwd)"
-exec docker run --rm \
-    --volume="$MOUNT:/srv/jekyll:Z" \
-    --publish 127.0.0.1:4000:4000 \
-    jekyll/jekyll \
-    jekyll serve
